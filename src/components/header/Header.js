95ce3365cf6a52a03fdd16d11864cc78b97c5524
Behzod <89260874+behzodk@users.noreply.github.com>
2023-10-12 10:57:32 +0500

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,21 +7,6 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 
 const Header = () => {
-    useEffect(() => {
-        window.addEventListener("scroll", isSticky);
-        return () => {
-          window.removeEventListener("scroll", isSticky);
-        };
-      }, []);
-    
-      const isSticky = (e) => {
-        const header = document.querySelector(".header");
-        const scrollTop = window.scrollY;
-        scrollTop >= 20
-          ? header.classList.add("is-sticky")
-          : header.classList.remove("is-sticky");
-      };
-
       const [anchorEl, setAnchorEl] = React.useState(null);
       const open = Boolean(anchorEl);
       const handleClick = (event) => {
